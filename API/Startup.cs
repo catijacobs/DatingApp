1400aecbce95f4aa3599eb3007577cab0640ec68
Catia Jacob <catia.jacob@inovflow.local>
2022-06-02 18:51:02 +0100

    createFirstEntity

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,10 +28,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,9 +36,7 @@ namespace API
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                app.UseDeveloperExceptionPage();  
             }
 
             app.UseHttpsRedirection();
